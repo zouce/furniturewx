03d6f79890e926a19ee642c1b0b36a6e96f10bd9
zouce <zoucece@sina.com>
2022-08-17 12:12:39 +0800

    startproject

---
+++ furniturewx/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for furniturewx project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'furniturewx.settings')
+
+application = get_asgi_application()
