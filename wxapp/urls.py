e213ae03a5d22b05ca5f057c2ab7291000df6638
zouce <zoucece@sina.com>
2022-08-23 19:21:24 +0800

    add classify

--- wxapp/urls.py
+++ wxapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from wxapp.views import index
+from wxapp.views import index, classify, tips
 
 
 urlpatterns = [
     path('',index),
+    path('classify',classify),
+    path('tips',tips),
 ]
--- wxapp/views.py
+++ wxapp/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+import json
+import base64
+import requests
+import os
+from pathlib import Path
 
 
 def index(request):
     return render(request, "web.html")
 
+def classify(request):
+
+    data = json.loads(request.body)
+    img_base64 = data.get('img_base64')
+    img = base64.b64decode(img_base64)
+    img_url = os.path.join('/home/zouce/furniturewx/static/','classify/test1.jpg')
+    with open(img_url,'wb') as f:
+        f.write(img)
+    #print(request.body)
+    #print(data)
+    #print(data.get('img'))
+    print(1)
+    params = {
+            'img_base64': img_base64,
+            # 'test': 111,
+        }
+    response = requests.get('http://furnitureclassify.free.idcfengye.com',params=params).json()
+    print(response['result'])
+    return JsonResponse({'result':'success','name':'classify'})
+
+def tips(request):
+    return JsonResponse({'result':'success','name':'tips'})
